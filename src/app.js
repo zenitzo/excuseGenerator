611056952f35dc6ecd72ba513f96f7a6dc8f0c2f
Chris Gonzalez <107893812+zenitzo@users.noreply.github.com>
2024-12-13 21:21:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuses = [
+    "My dog ate my homework.",
+    "I forgot it at home.",
+    "The internet was down.",
+    "Aliens abducted my project.",
+    "I had a flat tire.",
+    "My cat spilled coffee on it.",
+    "I overslept.",
+    "The power went out.",
+    "I was abducted by ninjas.",
+    "Traffic was insane."
+  ];
+
+  const generateExcuse = () => {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    return excuses[randomIndex];
+  };
+
+  const button = document.getElementById("generate-btn");
+  const excuseText = document.getElementById("excuse");
+
+  button.addEventListener("click", () => {
+    excuseText.innerText = generateExcuse();
+  });
+
+  console.log("Excuse generator loaded successfully!");
 };
