75882d3679dd9fe486df2cd99d7e5fb7527c80ea
zenitzo <christiang.dev@gmail.com>
2025-01-27 19:39:58 -0500

    split arrays for dynamic generation

--- src/app.js
+++ src/app.js
@@ -1,29 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  const excuses = [
-    "My dog ate my homework.",
-    "I forgot it at home.",
-    "The internet was down.",
-    "Aliens abducted my project.",
-    "I had a flat tire.",
-    "My cat spilled coffee on it.",
-    "I overslept.",
-    "The power went out.",
-    "I was abducted by ninjas.",
-    "Traffic was insane."
+window.onload = function () {
+  // Split excuses into parts 
+  const subjects = [
+    "My dog",
+    "My cat",
+    "My neighbor",
+    "Aliens",
+    "My grandma",
+    "My friend",
+    "A random stranger",
+  ];
+  const actions = [
+    "ate",
+    "destroyed",
+    "spilled coffee on",
+    "abducted",
+    "peed on",
+    "forgot about",
+    "borrowed",
+  ];
+  const objects = [
+    "my homework",
+    "my project",
+    "my car keys",
+    "the internet connection",
+    "my shoes",
+    "my lunch",
+    "my bike",
+  ];
+  const times = [
+    "right before the deadline",
+    "this morning",
+    "last night",
+    "during lunch",
+    "while I was sleeping",
+    "while I was on a call",
+    "just now",
   ];
 
+  // Function to generate a random excuse
   const generateExcuse = () => {
-    const randomIndex = Math.floor(Math.random() * excuses.length);
-    return excuses[randomIndex];
+    const randomIndex = (array) => Math.floor(Math.random() * array.length);
+
+    return (
+      subjects[randomIndex(subjects)] +
+      " " +
+      actions[randomIndex(actions)] +
+      " " +
+      objects[randomIndex(objects)] +
+      " " +
+      times[randomIndex(times)]
+    );
   };
 
+  // Attach the click event to the button
   const button = document.getElementById("generate-btn");
   const excuseText = document.getElementById("excuse");
 
